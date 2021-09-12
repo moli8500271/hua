0f0de0c8907a35b4894f08aecaae3b906e859a38
wgh <fafa13718228720@126.com>
2021-09-12 21:55:34 +0800

    试炼

---
+++ src/components/child.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+          
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+         name:"showMage"   
+        }
+    },
+    methods:{
+        shows(){
+            console.log("我是oppo");
+        }
+    }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+     <!-- 1.定义组件 2.渲染组件表 -->
+     <input type="button" value="获取元素" @click="btn"/>
+      <h3 id="h33" ref="myh3">zwwasdsdsdf</h3>
+       
+  </div>
+</template>
+<script>
+export default {
+  
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+     btn(){
+       //用dom获取
+      //  console.log(document.getElementById('h33').innerText);
+      let aa=this.$refs.myh3.innerText;
+      console.log(aa);
+     }
+  }
+}
+</script>
