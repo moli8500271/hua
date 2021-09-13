3cbce69097867335fb1ade8aeec19a4e06296ac7
wgh <fafa13718228720@126.com>
2021-09-13 10:47:28 +0800

    xxx

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
      <!-- 1.定义组件 2.渲染组件表 -->
      <input type="button" value="获取元素" @click="btn"/>
       <h3 id="h33" ref="myh3">zwwasdsdsdf</h3>
-       <div>zww322</div>
+       <div>zww322nbvnb</div>
   </div>
 </template>
 <script>
