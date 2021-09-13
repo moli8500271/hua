8617767b7630474cd8f034dee15de56af26bbecf
wgh <fafa13718228720@126.com>
2021-09-13 10:58:31 +0800

    qwer

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
      <!-- 1.定义组件 2.渲染组件表 -->
      <input type="button" value="获取元素" @click="btn"/>
       <h3 id="h33" ref="myh3">zwwasdsdsdf</h3>
-       <div>zww322asdfnbvnb</div>
+       <div>qwerqrewrerqw</div>
   </div>
 </template>
 <script>
